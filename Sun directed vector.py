559cbc95a2739339f64e25e9e0da3c463e04ea18
Gleb Belyy <79268622+GBelyy@users.noreply.github.com>
2021-08-05 14:30:24 +0300

    Update Sun directed vector.py

--- Sun directed vector.py
+++ Sun directed vector.py
@@ -11,12 +11,10 @@ AU = 149597870.7  # астрономическая единица
 # Paris location
 #  longitude = 2.35
 #  latitude = 48.86
-#  time zone = UTC+3
 #  16/08/2021,12:00:00
 
 longitude = float(input("Local longitude of POI,degrees "))
 latitude = float(input("Local latitude of POI,degrees "))
-zone = float(input("Type time zone in UTC: "))
 epoch = [float(item) for item in reg.split(r'[,/:]', input("Epoch in UTC format "))]
 
 #  epoch -> julian date
@@ -56,29 +54,62 @@ print("Unit vector of sun direction is (in AU)")
 print([round(i,5) for i in r_vec/np.linalg.norm(r_vec)])
 
 # Sun elevation angle
-# Local Standard Time Meridian (LSTM)
-LSTM = 15 * zone
+C = 360  # 2pi radians in degrees
+td = 24  # length of a day in hours
+tilt = 23.5  # tilt of Earth on its axes
+d_year = 365  # length of a year in days
+d_sol = 173  #Julian day of Summer Solstice
 
-# Equation of Time (EoT)
-B = 360/365 * (JD - Jul_date(1,1,epoch[2],0,0,0) - 81)
-EoT = 9.87 * mth.sin(mth.radians(2*B)) - 7.53 * mth.cos(mth.radians(B)) - 1.5 * mth.sin(mth.radians(B))
+days = JD - Jul_date(1,1,epoch[2],epoch[3],epoch[4],epoch[5]) + 1  # Julian day of a current day from the beginning of the year
+time = epoch[3] + (epoch[4] + epoch[5]/60)/60  # time in UTC
+decl = mth.radians(tilt * mth.cos(mth.radians(C*(days - d_sol)/d_year))) # solar declination angle
+theta = mth.asin(mth.sin(latitude)*mth.sin(decl) - mth.cos(latitude)\
+                 *mth.cos(decl)*mth.cos(mth.radians(C*time/td) - longitude)) * 180/mth.pi
 
-# Time Correction Factor (TC),minutes
-TC = 4*(longitude - LSTM) + EoT
-
-# Local Solar Time (LST),hours
-LST = epoch[3] + (epoch[4] + epoch[5]/60)/60 + TC/60
-
-# Hour Angle (HRA),degrees
-HRA = 15 * (LST - 12)
-
-# Declination angle
-decl = -23.45 * mth.cos(360/365 * (JD - Jul_date(1,1,epoch[2],0,0,0) + 10))
-
-#Elevation angle
-theta = mth.degrees(mth.asin(mth.sin(mth.radians(decl)) * mth.sin(mth.radians(latitude)) \
-        + mth.cos(mth.radians(decl)) * mth.cos(mth.radians(latitude)) * mth.cos(mth.radians(HRA))))
 print('Sun elevation angle is ', round(theta, 5), 'degrees')
 
+""""
+#График из статьи
 
+longitude = radians(2.35) # float(input("Local longitude of POI,degrees "))
+latitude = radians(48.86) # float(input("Local latitude of POI,degrees "))
+epoch = [float(item) for item in reg.split(r'[,/:]', '16/08/2021,00:00:00')]
+C = 360  # 2pi radians in degrees
+td = 24  # length of a day in hours
+tilt = 23.5  # tilt of Earth on its axes
+d_year = 365  # length of a year in days
+d_sol = 173  # Julian day of Summer Solstice
 
+JD = Jul_date(epoch[0],epoch[1],epoch[2],epoch[3],epoch[4],epoch[5]) - Jul_date(1,1,epoch[2],epoch[3],epoch[4],epoch[5]) + 1
+t = []
+angle = []
+for i in range(0,25):
+    time = i + (epoch[4] + epoch[5]/60)/60
+    decl = radians(tilt * cos(radians(C*(JD - 173)/365)))
+    theta = asin(sin(latitude)*sin(decl) - cos(latitude)*cos(decl)*cos(radians(C*time/24) - longitude)) * 180/pi
+    angle.append(theta)
+    t.append(i)
+fig, ax = plt.subplots()
+ax.plot(t, angle)
+ax.set_xlabel('Time at 16 Avg, hours')
+ax.set_ylabel('Sun elevation angle, degrees')
+ax.set_xlim([min(t), max(t)])
+ax.set_ylim([round(min(angle)-5,-1), floor(max(angle)+5)])
+ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(2))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(10))
+ax.yaxis.set_minor_locator(ticker.MultipleLocator(2))
+
+ax.grid(which='major',
+        color = 'k')
+
+ax.grid(which='minor',
+        color = 'gray',
+        linestyle = ':')
+        
+ax.minorticks_on()
+fig.set_figwidth(8)
+fig.set_figheight(8)
+
+plt.show()
+"""
